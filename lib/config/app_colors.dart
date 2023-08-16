9f82df8336dae35e86c795950f19b3b436b37c12
helly <luungoclan125690@gmail.com>
2023-08-16 17:46:41 +0700

    feat: add base config

---
+++ lib/config/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const primary = Color(0xFF16171D);
+  static const acccent = Color(0xFFF5C249);
+  static const secondary = Color(0xFF21242D);
+  static const placeHolder = Color(0xFFA7AEBF);
+  static const textColor = Color(0xFFF8F8F8);
+}
---
+++ lib/config/app_constants.dart
@@ -0,0 +1,4 @@
+//define all fit value in here: width, height, number, ratio,...
+class AppConstants {
+  const AppConstants._();
+}
